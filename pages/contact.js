e358933bb6d3029bb8a1d7f80c515298e968458b
James Moorhouse <jamesmoorhouse@live.co.uk>
2019-03-11 22:10:32 +0000

    added extra links to contact

--- pages/contact.js
+++ pages/contact.js
@@ -9,6 +9,11 @@ const ContactPage = () => {
         <p>jamesmoorhouse@live.co.uk</p>
         <h2 className={genericStyles.secondaryTitle}>Phone:</h2>
         <p>07540306772</p>
+        <h1 className={genericStyles.title}>Connect</h1><br/>
+        <h2 className={genericStyles.secondaryTitle}>LinkedIn:</h2>
+        <p><a href='https://www.linkedin.com/in/james-moorhouse-96319858/'>My Profile</a> </p>
+        <h2 className={genericStyles.secondaryTitle}>GitHub:</h2>
+        <p><a href='https://github.com/jjmoorhouse/jamesmoorhouse.co.uk'>Repository for this website</a></p>
     </Fragment>
 }
 
